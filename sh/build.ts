b5cea405da99208c07db119a0b722f891a6a0d0f
Rui Costa <ruicosta042@gmail.com>
2023-06-06 20:20:56 +0100

    chore: update deps, security fixes

--- sh/build.ts
+++ sh/build.ts
@@ -1,4 +1,4 @@
-import "zx/globals";
+import { $ } from "@tuplo/shell";
 
 async function main() {
 	await $`rm -rf dist`;
@@ -7,8 +7,8 @@ async function main() {
 	await $`rm -rf dist/helpers`;
 
 	const flags = ["--bundle", "--platform=node", "--minify"];
-	await $`esbuild src/cjs/index.js --format=cjs ${flags} --outfile=dist/index.cjs`;
-	await $`esbuild src/index.ts --format=esm ${flags} --outfile=dist/index.mjs`;
+	await $`esbuild src/cjs/index.js --format=cjs --outfile=dist/index.cjs ${flags}`;
+	await $`esbuild src/index.ts --format=esm --outfile=dist/index.mjs ${flags}`;
 }
 
 main();
